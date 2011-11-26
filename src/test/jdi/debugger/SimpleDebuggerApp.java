b8dcbd142370b2205c420ccc87fb8a1af46d2024
stepan <none@none>
2011-11-26 15:45:10 +0100

    Javadoc problem

--- src/test/jdi/debugger/SimpleDebuggerApp.java
+++ src/test/jdi/debugger/SimpleDebuggerApp.java
@@ -1,24 +1,81 @@
 package test.jdi.debugger;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import com.sun.jdi.Bootstrap;
+import com.sun.jdi.ThreadReference;
+import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.VirtualMachineManager;
 
 public class SimpleDebuggerApp {
 
-	VirtualMachineManager vmm;
+	private static final int port = 52313;
+	private static final String cmd = "C:\\Programs\\Java\\jdk1.6.0_22\\bin\\javaw.exe"
+			+ " -agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:"
+			+ port
+			+ " -Dfile.encoding=Cp1252 -classpath"
+			+ " C:\\Users\\stepan\\Data\\workspaces\\mthesis\\jdi-test\\bin;C:\\Programs\\Java\\jdk1.6.0_22\\lib\\tools.jar"
+			+ " test.jdi.debuggee.SimpleIntApp";
+	private VirtualMachineManager vmm;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Main program = new Main();
-		
-		program.init();
+		SimpleDebuggerApp program = new SimpleDebuggerApp();
+
+		try {
+			program.init();
+		} catch (IOException e) {
+			throw new RuntimeException("Program ended", e);
+		}
 	}
-	
-	private void init() {
+
+	private static class StreamGobbler implements Runnable {
+
+		private BufferedReader br;
+
+		public StreamGobbler(InputStream is) {
+			br = new BufferedReader(new InputStreamReader(is));
+		}
+
+		@Override
+		public void run() {
+			String line;
+			try {
+				while ((line = br.readLine()) != null) {
+					System.out.println("Gobblered line: " + line);
+				}
+			} catch (IOException e) {
+				System.err.println("Gobbler ended");
+			}
+
+		}
+
+	}
+
+	private void init() throws IOException {
 		vmm = Bootstrap.virtualMachineManager();
-		
-		vmm.
+
+		vmm.defaultConnector();
+
+		ProcessBuilder pb = new ProcessBuilder(cmd);
+		Process process = pb.start();
+
+		Thread processStdoutGobbler = new Thread(new StreamGobbler(
+				process.getInputStream()));
+		processStdoutGobbler.start();
+
+		// TODO change null to a SocketConnection
+		VirtualMachine vm = vmm.createVirtualMachine(null, process);
+
+		for (ThreadReference threadReference : vm.allThreads()) {
+			// threadReference.
+		}
+
 	}
 
 }
