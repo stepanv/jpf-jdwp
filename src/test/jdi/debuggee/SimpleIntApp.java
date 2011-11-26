075a2e474317c1b801a6daffb5248fb6e904fe00
stepan <none@none>
2011-11-26 18:25:34 +0100

    Getting value of an static field works.

--- src/test/jdi/debuggee/SimpleIntApp.java
+++ src/test/jdi/debuggee/SimpleIntApp.java
@@ -1,21 +1,41 @@
 package test.jdi.debuggee;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 public class SimpleIntApp {
 
 	public static int number = 0;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		System.out.println("initial number value: " + number);
 
 		number = new Random().nextInt();
-		
+
+		String currentTime  = SimpleDateFormat.getTimeInstance(SimpleDateFormat.LONG).format(new Date());
+		System.out.println(currentTime);
 		System.out.println("randomized number value: " + number);
-		
+
+		File logFile = new File("c:\\tmp\\output.txt");
+
+		try {
+			Writer writer = new FileWriter(logFile, true);
+			writer.append("\n" + currentTime);
+			writer.append("\nrandomized number value: " + number);
+			writer.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+
 		try {
 			Thread.sleep(100000000);
 		} catch (InterruptedException e) {
--- src/test/jdi/debugger/SimpleDebuggerApp.java
+++ src/test/jdi/debugger/SimpleDebuggerApp.java
@@ -4,21 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 
 import com.sun.jdi.Bootstrap;
+import com.sun.jdi.Field;
+import com.sun.jdi.IncompatibleThreadStateException;
+import com.sun.jdi.ReferenceType;
+import com.sun.jdi.StackFrame;
 import com.sun.jdi.ThreadReference;
+import com.sun.jdi.Value;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.VirtualMachineManager;
+import com.sun.jdi.connect.Connector.Argument;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.Event;
+import com.sun.jdi.event.VMStartEvent;
 
 public class SimpleDebuggerApp {
 
-	private static final int port = 52313;
-	private static final String cmd = "C:\\Programs\\Java\\jdk1.6.0_22\\bin\\javaw.exe"
-			+ " -agentlib:jdwp=transport=dt_socket,suspend=y,address=localhost:"
-			+ port
-			+ " -Dfile.encoding=Cp1252 -classpath"
-			+ " C:\\Users\\stepan\\Data\\workspaces\\mthesis\\jdi-test\\bin;C:\\Programs\\Java\\jdk1.6.0_22\\lib\\tools.jar"
-			+ " test.jdi.debuggee.SimpleIntApp";
 	private VirtualMachineManager vmm;
 
 	/**
@@ -30,6 +35,14 @@ public class SimpleDebuggerApp {
 		try {
 			program.init();
 		} catch (IOException e) {
+			throw new RuntimeException("Program ended", e);
+		} catch (IllegalConnectorArgumentsException e) {
+			throw new RuntimeException("Program ended", e);
+		} catch (VMStartException e) {
+			throw new RuntimeException("Program ended", e);
+		} catch (InterruptedException e) {
+			throw new RuntimeException("Program ended", e);
+		} catch (IncompatibleThreadStateException e) {
 			throw new RuntimeException("Program ended", e);
 		}
 	}
@@ -57,23 +70,70 @@ public class SimpleDebuggerApp {
 
 	}
 
-	private void init() throws IOException {
+	private void init() throws IOException, IllegalConnectorArgumentsException,
+			VMStartException, InterruptedException,
+			IncompatibleThreadStateException {
 		vmm = Bootstrap.virtualMachineManager();
 
 		vmm.defaultConnector();
 
-		ProcessBuilder pb = new ProcessBuilder(cmd);
-		Process process = pb.start();
+		LaunchingConnector lc = vmm.defaultConnector();
+		Map<String, Argument> args = lc.defaultArguments();
+		args.get("main").setValue("test.jdi.debuggee.SimpleIntApp");
+		args.get("options")
+				.setValue(
+						"-classpath C:\\Users\\stepan\\Data\\workspaces\\mthesis\\jdi-test\\bin");
+		VirtualMachine vm = lc.launch(args);
 
-		Thread processStdoutGobbler = new Thread(new StreamGobbler(
-				process.getInputStream()));
+		while (true) {
+			boolean vmStartEventReceived = false;
+			for (Event event : vm.eventQueue().remove()) {
+				if (event instanceof VMStartEvent) {
+					System.out.println("VM started");
+					vmStartEventReceived = true;
+					break;
+				} else {
+					System.out.println("received event: " + event);
+				}
+			}
+			if (vmStartEventReceived) {
+				break;
+			}
+		}
+
+		Thread processStdoutGobbler = new Thread(new StreamGobbler(vm.process()
+				.getInputStream()));
 		processStdoutGobbler.start();
+		Thread processErrorGobbler = new Thread(new StreamGobbler(vm.process()
+				.getErrorStream()));
+		processErrorGobbler.start();
 
-		// TODO change null to a SocketConnection
-		VirtualMachine vm = vmm.createVirtualMachine(null, process);
+		vm.resume();
 
 		for (ThreadReference threadReference : vm.allThreads()) {
-			// threadReference.
+			threadReference.suspend();
+			System.out.println(threadReference.name());
+			for (StackFrame frame : threadReference.frames()) {
+				System.out.println(frame.toString());
+			}
+			threadReference.resume();
+		}
+
+		Field field;
+		Value value = null;
+		for (ReferenceType referenceType : vm.allClasses()) {
+			String name = referenceType.name();
+			if (name.contains("SimpleIntApp")) {
+				field = referenceType.fieldByName("number");
+				
+				value = referenceType.getValue(field);
+			}
+		}
+
+		if (value != null) {
+		System.out.println("Found value: " + value.toString());
+		} else {
+			System.out.println("NOTHING FOUND...");
 		}
 
 	}
