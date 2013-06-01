8a13272ed3bc95141b592cec95fda8baa880ef1f
stepan <vavra.stepan@gmail.com>
2013-06-01 11:19:05 +0200

    Created util jpf jdwp classes for Line and Variable tables.
    --HG--
    branch : redesign-v7

--- gov.nasa.jpf.jdwp/src/gov/nasa/jpf/jdwp/command/MethodCommand.java
+++ gov.nasa.jpf.jdwp/src/gov/nasa/jpf/jdwp/command/MethodCommand.java
@@ -1,11 +1,11 @@
 package gov.nasa.jpf.jdwp.command;
 
-import gnu.classpath.jdwp.util.LineTable;
-import gnu.classpath.jdwp.util.VariableTable;
 import gov.nasa.jpf.jdwp.VirtualMachineHelper;
 import gov.nasa.jpf.jdwp.exception.JdwpError;
 import gov.nasa.jpf.jdwp.exception.JdwpError.ErrorType;
 import gov.nasa.jpf.jdwp.id.type.ReferenceTypeId;
+import gov.nasa.jpf.jdwp.util.LineTable;
+import gov.nasa.jpf.jdwp.util.VariableTable;
 import gov.nasa.jpf.vm.ClassInfo;
 import gov.nasa.jpf.vm.MethodInfo;
 
@@ -14,24 +14,40 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public enum MethodCommand implements Command, ConvertibleEnum<Byte, MethodCommand> {
+
+	/**
+	 * <p>
+	 * <h2>JDWP Specification</h2>
+	 * Returns line number information for the method, if present. The line
+	 * table maps source line numbers to the initial code index of the line. The
+	 * line table is ordered by code index (from lowest to highest). The line
+	 * number information is constant unless a new class definition is installed
+	 * using {@link VirtualMachineCommand#REDEFINECLASSES}.
+	 * </p>
+	 */
 	LINETABLE(1) {
 		@Override
 		public void execute(MethodInfo methodInfo, ByteBuffer bytes, DataOutputStream os, CommandContextProvider contextProvider) throws IOException, JdwpError {
-			
-			   LineTable lt = LineTable.factory(methodInfo); // TODO do this in a uniform way (see method bellow)
-			   
-//			    LineTable lt = method.getLineTable();
-			    lt.write(os);
-
+			// TODO Has to be verified, that JPF has always sorted all
+			// instructions according to their index otherwise the specification
+			// is violated
+			LineTable lineTable = new LineTable(methodInfo);
+			lineTable.write(os);
 		}
 	},
+
+	/**
+	 * Returns variable information for the method. The variable table includes
+	 * arguments and locals declared within the method. For instance methods,
+	 * the "this" reference is included in the table. Also, synthetic variables
+	 * may be present.
+	 */
 	VARIABLETABLE(2) {
 		@Override
 		public void execute(MethodInfo methodInfo, ByteBuffer bytes, DataOutputStream os, CommandContextProvider contextProvider) throws IOException, JdwpError {
 
-			    VariableTable variableTable = new VariableTable(methodInfo); // TODO do this in a different uniform way
-//			    VariableTable vt = method.getVariableTable();
-			    variableTable.write(os);
+			VariableTable variableTable = new VariableTable(methodInfo);
+			variableTable.write(os);
 
 		}
 	},
@@ -74,13 +90,14 @@ public enum MethodCommand implements Command, ConvertibleEnum<Byte, MethodComman
 		return map.get(val);
 	}
 
-	public abstract void execute(MethodInfo methodInfo, ByteBuffer bytes, DataOutputStream os, CommandContextProvider contextProvider) throws IOException, JdwpError;
-	
+	public abstract void execute(MethodInfo methodInfo, ByteBuffer bytes, DataOutputStream os, CommandContextProvider contextProvider) throws IOException,
+			JdwpError;
+
 	@Override
 	public void execute(ByteBuffer bytes, DataOutputStream os, CommandContextProvider contextProvider) throws IOException, JdwpError {
-		 ReferenceTypeId refId = contextProvider.getObjectManager().readReferenceTypeId(bytes);
-		    ClassInfo clazz = refId.get();
+		ReferenceTypeId refId = contextProvider.getObjectManager().readReferenceTypeId(bytes);
+		ClassInfo clazz = refId.get();
 
-		    execute(VirtualMachineHelper.getClassMethod(clazz, bytes.getLong()), bytes, os, contextProvider);
+		execute(VirtualMachineHelper.getClassMethod(clazz, bytes.getLong()), bytes, os, contextProvider);
 	}
 }
---
+++ gov.nasa.jpf.jdwp/src/gov/nasa/jpf/jdwp/util/VariableTable.java
@@ -0,0 +1,189 @@
+package gov.nasa.jpf.jdwp.util;
+
+import gov.nasa.jpf.jdwp.value.StringRaw;
+import gov.nasa.jpf.jvm.ClassFile;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.LocalVarInfo;
+import gov.nasa.jpf.vm.MethodInfo;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author stepan
+ * 
+ */
+public class VariableTable {
+
+	/**
+	 * The number of words in the frame used by arguments. Eight-byte arguments
+	 * use two words; all others use one.
+	 */
+	int argCnt;
+
+	private List<Slot> slots = new ArrayList<VariableTable.Slot>();
+
+	public VariableTable(MethodInfo methodInfo) {
+		System.out.println("VARIABLE TABLE CREATION: method: " + methodInfo);
+
+		for (LocalVarInfo localVarInfo : methodInfo.getLocalVars()) {
+			new Slot(localVarInfo, methodInfo);
+		}
+		argCnt = methodInfo.getArgumentsSize(); // TODO this might be wrong ..
+												// see a comment bellow
+		// according to JDWP argCount is:
+		// The number of words in the frame used by arguments. Eight-byte
+		// arguments use two words; all others use one.
+		// I'm so unsure what exactly this means ... just TODO - has to be
+		// tested
+	}
+	
+	public void write (DataOutputStream os) throws IOException {
+		os.writeInt(argCnt);
+		os.writeInt(slots.size());
+		
+		for (Slot slot : slots) {
+			slot.write(os);
+		}
+	}
+
+	private class Slot {
+
+		/**
+		 * First code index at which the variable is visible, The variable can
+		 * be get or set only when the current <code>codeIndex <= </code>
+		 * current frame code index <code> < codeIndex + lenth</code>
+		 */
+		private long codeIndex;
+
+		/**
+		 * The variable's name.
+		 */
+		private String name;
+
+		/**
+		 * The variable type's JNI signature.
+		 */
+		private String signature;
+
+		/**
+		 * Unsigned value used in conjunction with <code>codeIndex</code>.
+		 */
+		private int length;
+
+		/**
+		 * The local variable's index in its frame
+		 */
+		private int slot;
+
+		/**
+		 * Creates and registers a slot in a {@link VariableTable} instance.
+		 * 
+		 * @param localVarInfo
+		 *            An instance of {@link LocalVarInfo} from JPF.
+		 * @param methodInfo
+		 *            An instance of {@link MethodInfo} which this local
+		 *            variable belongs to
+		 */
+		public Slot(LocalVarInfo localVarInfo, MethodInfo methodInfo) {
+
+			// we need to calculate instruction index from accumulated bytecode
+			// position
+			Instruction startInstruction = methodInstructionDownToPosition(methodInfo, localVarInfo.getStartPC());
+			Instruction endInstruction = methodInstructionUpToPosition(methodInfo, localVarInfo.getStartPC() + localVarInfo.getLength() - 1);
+
+			codeIndex = startInstruction.getInstructionIndex();
+			name = localVarInfo.getName();
+			signature = localVarInfo.getSignature();
+			length = (int) (endInstruction.getInstructionIndex() - codeIndex) + 1;
+			slot = localVarInfo.getSlotIndex();
+
+			slots.add(this);
+
+			System.out.println("VARIABLE TABLE: index: " + codeIndex + " slot: " + slot + " length: " + length + " name: " + name + " ... localVarInfo: "
+					+ localVarInfo);
+		}
+
+		/**
+		 * Finds an instruction at position which closest from the bottom to the
+		 * position given as a parameter.
+		 * 
+		 * TODO [for PJA] For some reason debug table might return endPC or
+		 * startPC that doesn't contain an instruction
+		 * 
+		 * @see MethodInfo#getInstructionAt(int)
+		 * @see ClassFile#parseLocalVarTableAttr(gov.nasa.jpf.classfile.ClassFileReader,
+		 *      Object)
+		 * 
+		 *      TODO handle errors properly
+		 * 
+		 * @param methodInfo
+		 * @param position
+		 * @return
+		 */
+		private Instruction methodInstructionUpToPosition(MethodInfo methodInfo, int position) {
+			Instruction[] code = methodInfo.getInstructions();
+
+			Instruction instruction = code[0];
+
+			for (int i = 0; i < code.length; i++) {
+				if ((code[i] != null) && (code[i].getPosition() <= position)) {
+					instruction = code[i];
+				}
+			}
+
+			return instruction;
+		}
+
+		/**
+		 * Finds an instruction at position which closest from the top to the
+		 * position given as a parameter.
+		 * 
+		 * TODO [for PJA] For some reason debug table might return endPC or
+		 * startPC that doesn't contain an instruction
+		 * 
+		 * @see MethodInfo#getInstructionAt(int)
+		 * @see ClassFile#parseLocalVarTableAttr(gov.nasa.jpf.classfile.ClassFileReader,
+		 *      Object)
+		 * 
+		 *      TODO handle errors properly
+		 * 
+		 * @param methodInfo
+		 * @param position
+		 * @return
+		 */
+		private Instruction methodInstructionDownToPosition(MethodInfo methodInfo, int position) {
+			Instruction[] code = methodInfo.getInstructions();
+
+			Instruction instruction = code[code.length - 1];
+
+			for (int i = code.length - 1; i >= 0; --i) {
+				if ((code[i] != null) && (code[i].getPosition() >= position)) {
+					instruction = code[i];
+				}
+			}
+
+			return instruction;
+		}
+
+		/**
+		 * Outputs the slot into the stream.
+		 * 
+		 * @param os
+		 *            The stream where to output this slot.
+		 * @throws IOException
+		 *             If an I/O error occurs
+		 */
+		public void write(DataOutputStream os) throws IOException {
+			os.writeLong(codeIndex);
+			new StringRaw(name).write(os);
+			new StringRaw(signature).write(os);
+			os.writeInt(length);
+			os.writeInt(slot);
+		}
+	}
+
+}
