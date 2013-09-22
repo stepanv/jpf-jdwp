72e69de5221755305035ca53a0e96b7f864f7f1c
stepan <vavra.stepan@gmail.com>
2013-09-22 13:29:42 +0200

    more debug info.

--- src/main/gov/nasa/jpf/jdwp/JDWPListenerBase.java
+++ src/main/gov/nasa/jpf/jdwp/JDWPListenerBase.java
@@ -53,182 +53,200 @@ public class JDWPListenerBase implements VMListener {
 
   @Override
   public void vmInitialized(VM vm) {
-    logger.trace("Processing listener");
+    logger.trace("VM initialized");
 
   }
 
   @Override
   public void executeInstruction(VM vm, ThreadInfo currentThread, Instruction instructionToExecute) {
-    logger.trace("Processing listener");
+    logger.trace("Execute instruction '{}' in thread '{}'", instructionToExecute, currentThread);
 
   }
 
   @Override
   public void instructionExecuted(VM vm, ThreadInfo currentThread, Instruction nextInstruction, Instruction executedInstruction) {
-    logger.trace("Processing listener");
+    logger.trace("Instruction '{}' executed in thread '{}'. Next instruction: '{}'", executedInstruction, currentThread, nextInstruction);
 
   }
 
   @Override
   public void threadStarted(VM vm, ThreadInfo startedThread) {
-    logger.trace("Processing listener");
+    logger.trace("Thread started: {}", startedThread);
 
   }
 
   @Override
   public void threadBlocked(VM vm, ThreadInfo blockedThread, ElementInfo lock) {
-    logger.trace("Processing listener");
+    logger.trace("Thread blocked: {} on object: {}", blockedThread, lock);
 
   }
 
   @Override
   public void threadWaiting(VM vm, ThreadInfo waitingThread) {
-    logger.trace("Processing listener");
+    logger.trace("Thread waiting: {}", waitingThread);
 
   }
 
   @Override
   public void threadNotified(VM vm, ThreadInfo notifiedThread) {
-    logger.trace("Processing listener");
+    logger.trace("Thread notified: {}", notifiedThread);
 
   }
 
   @Override
   public void threadInterrupted(VM vm, ThreadInfo interruptedThread) {
-    logger.trace("Processing listener");
+    logger.trace("Thread interrupted: {}", interruptedThread);
 
   }
 
   @Override
   public void threadTerminated(VM vm, ThreadInfo terminatedThread) {
-    logger.trace("Processing listener");
+    logger.trace("Thread terminated: {}", terminatedThread);
 
   }
 
   @Override
   public void threadScheduled(VM vm, ThreadInfo scheduledThread) {
-    logger.trace("Processing listener");
+    logger.trace("Thread scheduled: {}", scheduledThread);
 
   }
 
   @Override
   public void loadClass(VM vm, ClassFile cf) {
-    logger.trace("Processing listener");
+    logger.trace("Class load: {}", cf);
 
   }
 
   @Override
   public void classLoaded(VM vm, ClassInfo loadedClass) {
-    logger.trace("Processing listener");
+    logger.trace("Class loaded: {}", loadedClass);
 
   }
 
   @Override
   public void objectCreated(VM vm, ThreadInfo currentThread, ElementInfo newObject) {
-    logger.trace("Processing listener");
+    logger.trace("Object created: {}", newObject);
 
   }
 
   @Override
   public void objectReleased(VM vm, ThreadInfo currentThread, ElementInfo releasedObject) {
-    logger.trace("Processing listener: {}", releasedObject);
+    logger.trace("Object released: {}", releasedObject);
 
   }
 
   @Override
   public void objectLocked(VM vm, ThreadInfo currentThread, ElementInfo lockedObject) {
-    logger.trace("Processing listener");
+    logger.trace("Object locked: {}", lockedObject);
 
   }
 
   @Override
   public void objectUnlocked(VM vm, ThreadInfo currentThread, ElementInfo unlockedObject) {
-    logger.trace("Processing listener");
+    logger.trace("Object unlocked: {}", unlockedObject);
 
   }
 
   @Override
   public void objectWait(VM vm, ThreadInfo currentThread, ElementInfo waitingObject) {
-    logger.trace("Processing listener");
+    logger.trace("Object wait: {}", waitingObject);
 
   }
 
   @Override
   public void objectNotify(VM vm, ThreadInfo currentThread, ElementInfo notifyingObject) {
-    logger.trace("Processing listener");
+    logger.trace("Object notify: {}", notifyingObject);
 
   }
 
   @Override
   public void objectNotifyAll(VM vm, ThreadInfo currentThread, ElementInfo notifyingObject) {
-    logger.trace("Processing listener");
+    logger.trace("Object notify all: {}", notifyingObject);
 
   }
 
   @Override
   public void gcBegin(VM vm) {
-    logger.trace("Processing listener");
+    logger.trace("GC BEGIN");
 
   }
 
   @Override
   public void gcEnd(VM vm) {
-    logger.trace("Processing listener");
+    logger.trace("GC END");
 
   }
 
   @Override
   public void exceptionThrown(VM vm, ThreadInfo currentThread, ElementInfo thrownException) {
-    logger.trace("Processing listener");
+    logger.trace("Exception '{}' thrown in thread: {}", thrownException, currentThread);
 
   }
 
   @Override
   public void exceptionBailout(VM vm, ThreadInfo currentThread) {
-    logger.trace("Processing listener");
+    logger.trace("Exception bailout in thread: {}", currentThread);
 
   }
 
   @Override
   public void exceptionHandled(VM vm, ThreadInfo currentThread) {
-    logger.trace("Processing listener");
+    logger.trace("Exception handled in thread: {}", currentThread);
 
   }
 
   @Override
   public void choiceGeneratorRegistered(VM vm, ChoiceGenerator<?> nextCG, ThreadInfo currentThread, Instruction executedInstruction) {
-    logger.trace("Processing listener");
+    logger.trace("Choice generator registered: {} for thread: {}, instruction: {}", nextCG, currentThread, executedInstruction);
 
   }
 
   @Override
   public void choiceGeneratorSet(VM vm, ChoiceGenerator<?> newCG) {
-    logger.trace("Processing listener");
+    logger.trace("Choice generator set: {}", newCG);
 
   }
 
   @Override
   public void choiceGeneratorAdvanced(VM vm, ChoiceGenerator<?> currentCG) {
-    logger.trace("Processing listener");
+    logger.trace("Choice generator advanced: {}", currentCG);
 
   }
 
   @Override
   public void choiceGeneratorProcessed(VM vm, ChoiceGenerator<?> processedCG) {
-    logger.trace("Processing listener");
+    logger.trace("Choice generator processed: {}", processedCG);
 
   }
 
   @Override
   public void methodEntered(VM vm, ThreadInfo currentThread, MethodInfo enteredMethod) {
-    logger.trace("Processing listener");
+    logger.trace("Method entered: {}", enteredMethod);
 
   }
 
   @Override
   public void methodExited(VM vm, ThreadInfo currentThread, MethodInfo exitedMethod) {
-    logger.trace("Processing listener");
+    logger.trace("Method exited: {}", exitedMethod);
 
+  }
+
+  /* (non-Javadoc)
+   * @see gov.nasa.jpf.vm.VMListener#objectExposed(gov.nasa.jpf.vm.VM, gov.nasa.jpf.vm.ThreadInfo, gov.nasa.jpf.vm.ElementInfo, gov.nasa.jpf.vm.ElementInfo)
+   */
+  @Override
+  public void objectExposed(VM vm, ThreadInfo currentThread, ElementInfo sharedObject, ElementInfo exposedObject) {
+    logger.trace("Object exposed: '{}' .. shared object: '{}' in thread '{}'", exposedObject, sharedObject, currentThread);
+    
+  }
+
+  /* (non-Javadoc)
+   * @see gov.nasa.jpf.vm.VMListener#objectShared(gov.nasa.jpf.vm.VM, gov.nasa.jpf.vm.ThreadInfo, gov.nasa.jpf.vm.ElementInfo)
+   */
+  @Override
+  public void objectShared(VM vm, ThreadInfo currentThread, ElementInfo sharedObject) {
+    logger.trace("Object shared: '{}' in thread '{}'", sharedObject, currentThread);
+    
   }
 
 }
